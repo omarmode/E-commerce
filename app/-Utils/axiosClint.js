93431560f62db8b354beb1ac1016e9a7ecf8dcd0
omarmode <omarmode35@gmail.com>
2024-04-30 14:55:41 +0300

    first

--- app/-Utils/axiosClint.js
+++ app/-Utils/axiosClint.js
@@ -1,8 +1,8 @@
 const { default: axios } = require("axios");
 
 const apikey = process.env.NEXT_PUBLIC_REST_API_KEY;
-// const apiUrl = "http://localhost:1337/api";//old
-const apiUrl = "https://my-app-strapi.onrender.com/api";
+// const apiUrl = "http://localhost:1337/api"; //old
+const apiUrl = "https://strapi-demo-app-7h6t.onrender.com/api";
 
 const axiosClint = axios.create({
   baseURL: apiUrl,
